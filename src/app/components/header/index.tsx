f260792ac67d2f3e7dabfbfa69999928c85aea14
aldokusuma31 <aldo.kusumawardana@gmail.com>
2024-06-21 07:04:50 +0700

    main

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -8,13 +9,20 @@ export default function Header() {
     return (
         <header className="flex justify-between items-center px-2">
             <div className="flex items-center space-x-2">
-                <span>By</span>
-                <span className="text-lg font-bold">AKW</span>
+                <Image 
+                className="shadow-xl"
+                src="/home.svg"
+                alt="logo"
+                width={30}
+                height={30}
+                priority
+                />
+                <span className="text-slate-900">by<span className="text-lg text-slate-900 font-bold">AKW</span></span>
             </div>
             <nav className="hidden md:flex md:pr-4 md:space-x-6 lg:space-x-10">
-                <Link href="/about" className="hover:text-primary-deep-blue">About</Link>
-                <Link href="/about" className="hover:text-primary-deep-blue">Portfolio</Link>
-                <Link href="/about" className="hover:text-primary-deep-blue">Contact</Link>
+                <Link href="/about" className="text-slate-900 hover:text-primary-deep-blue">About</Link>
+                <Link href="/portfolio" className=" text-slate-900 hover:text-primary-deep-blue">Portfolio</Link>
+                <Link href="/contact" className=" text-slate-900 hover:text-primary-deep-blue">Contact</Link>
             </nav>
         </header>
     );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="relative flex flex-col min-h-screen bg-sky-200 z-20">
-          <div className="relative bg-sky-300 w-full z-50 mx-auto px-4 py-3 md:py-5 lg:px-10">
+        <div className="relative flex flex-col min-h-screen bg-orange-100 z-20">
+          <div className="relative bg-orange-200 w-full z-50 mx-auto px-4 py-3 md:py-5 lg:px-10">
             <Header />
           </div>
-          <main>
+          <main className="text-slate-900">
             {children}
           </main>
         </div>
